22f1b9f1d42317888ddefe586adc0ccea7bb54b5
Emma <emmalanza@hotmail.com>
2025-04-24 20:49:20 +0200

    improving list and header

--- src/components/cards/TradableCardsGrid.jsx
+++ src/components/cards/TradableCardsGrid.jsx
@@ -4,6 +4,7 @@ import { getTradableCards } from '@services/cards/cardService';
 import SearchBar from "@components/filters/SearchBar";
 import Card from "@components/cards/Card";
 import Filter from "@components/filters/Filter";
+import { CheckCircle } from 'lucide-react';
 
 const TradableCardsGrid = ({ onAddOrRemoveCard, currentCards }) => {
 
@@ -23,7 +24,6 @@ const TradableCardsGrid = ({ onAddOrRemoveCard, currentCards }) => {
         "DIAMOND_4",
         "STAR_1",
     ];
-
 
     useEffect(() => {
         const loadTradableCards = async () => {
@@ -61,16 +61,13 @@ const TradableCardsGrid = ({ onAddOrRemoveCard, currentCards }) => {
         filterCards();
     }, [searchQuery, filter, cards]);
 
-
     if (isLoading) return <Spinner />;
     if (error) return <p>No se han podido cargar las cartas</p>;
 
-
     return (
-        <section>
-            <div className="w-full max-w-7xl px-10 py-4
-            flex flex-col justify-center items-center gap-2 md:gap-0 md:flex-row md:justify-between">
-                <h1 className="text-3xl text-primary font-bold italic">Genes Formidables</h1>
+        <>
+            <div className="w-full max-w-7xl px-10 py-4 flex flex-col justify-center items-center gap-2 md:gap-0 md:flex-row md:justify-between">
+                <h3 className="text-xl text-primary font-bold italic">Añade cartas a tu lista ✨</h3>
                 <div className="flex flex-col sm:flex-row items-center gap-2">
                     <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} placeholder={"Buscar carta por nombre"} />
                     <Filter filter={filter} setFilter={setFilter} options={rarity} />
@@ -85,16 +82,21 @@ const TradableCardsGrid = ({ onAddOrRemoveCard, currentCards }) => {
                             key={card.id}
                             className="relative group w-full flex justify-center items-center"
                         >
-           
+                            {/* Marca si ya está en la lista */}
+                            {alreadyInList && (
+                                <div className="absolute top-2 left-2 bg-green-500 text-white rounded-full p-1 shadow-md z-10">
+                                    <CheckCircle size={16} />
+                                </div>
+                            )}
+
                             <Card card={card} />
 
-                    
+                            {/* Overlay con botón */}
                             <div className="absolute inset-0 backdrop-blur-xs opacity-0 group-hover:opacity-100 transition-all duration-300 flex justify-center items-center rounded-lg">
                                 <button
                                     onClick={() => onAddOrRemoveCard(card.id, alreadyInList)}
                                     className={`px-6 py-2 rounded-lg text-white font-bold shadow-lg transition-all duration-300 transform 
-                                    hover:scale-105 ${alreadyInList ? 'bg-accent' : 'bg-green-400'
-                                    }`}
+                                        hover:scale-105 ${alreadyInList ? 'bg-accent' : 'bg-green-400'}`}
                                 >
                                     {alreadyInList ? 'Eliminar' : 'Añadir'}
                                 </button>
@@ -103,8 +105,7 @@ const TradableCardsGrid = ({ onAddOrRemoveCard, currentCards }) => {
                     );
                 })}
             </div>
-
-        </section>
+        </>
     );
 };
 
--- src/components/users/CardsList.jsx
+++ src/components/users/CardsList.jsx
@@ -1,121 +1,49 @@
 import React, { useState } from 'react';
-import { addCardToList, removeCardFromList, getUserCardLists } from '@services/cardLists/cardListService';
-import TradableCardsGrid from '@components/cards/TradableCardsGrid';
+import { Eye } from 'lucide-react';
+import CardListModal from '@components/users/CardsListModal';
 
-const CardsLists = ({ lists }) => {
-    const [selectedListId, setSelectedListId] = useState(null);
-    const [showGrid, setShowGrid] = useState(false);
-    const [updatedLists, setUpdatedLists] = useState(lists);
+const CardsLists = ({ lists = [] }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedList, setSelectedList] = useState(null);
 
-    const handleToggleDetails = (listId) => {
-        if (selectedListId === listId) {
-            setSelectedListId(null);
-            setShowGrid(false);
-        } else {
-            setSelectedListId(listId);
-            setShowGrid(false);
-        }
+    const openModal = (list) => {
+        setSelectedList(list);
+        setIsModalOpen(true);
     };
 
-    const handleToggleGrid = () => {
-        if (!selectedListId) return alert('Selecciona una lista primero');
-        setShowGrid(prev => !prev);
-    };
-
-    const refreshListsFromBackend = async () => {
-        const token = localStorage.getItem('token');
-        try {
-            const data = await getUserCardLists(token);
-            setUpdatedLists(data);
-        } catch (error) {
-            console.error('Error recargando listas:', error);
-            alert('Hubo un error al actualizar la lista desde el servidor');
-        }
-    };
-
-    const handleAddOrRemoveCard = async (cardId, isAdded) => {
-        const token = localStorage.getItem('token');
-        try {
-            if (isAdded) {
-                await removeCardFromList(selectedListId, cardId, token);
-            } else {
-                await addCardToList(selectedListId, cardId, token);
-            }
-
-            await refreshListsFromBackend();
-
-        } catch (error) {
-            console.error('Error actualizando carta:', error);
-            alert('Hubo un error al actualizar la carta');
-        }
+    const closeModal = () => {
+        setSelectedList(null);
+        setIsModalOpen(false);
     };
 
     return (
         <div className="p-4 md:p-8 min-w-full text-primary">
             <h2 className="text-2xl md:text-3xl font-bold mb-6">Tus Listas</h2>
 
-            <div className="space-y-6">
-                {updatedLists.map(list => (
-                    <div key={list.id} className="bg-white p-4 md:p-6 shadow-md rounded-lg relative">
-
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+                {Array.isArray(lists) && lists.map(list => (
+                    <div
+                        key={list.id}
+                        className="flex justify-between
+                        bg-white p-6 rounded-2xl shadow-md border border-blue-100 hover:shadow-lg transition"
+                    >
                         <h3 className="text-lg md:text-xl font-semibold">{list.listType}</h3>
-                        <p className="text-sm md:text-base text-gray-600">Cantidad de cartas: {list.cards.length}</p>
-
-                        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mt-4">
-                            <button
-                                onClick={() => handleToggleDetails(list.id)}
-                                className="w-full md:w-auto bg-primary text-white px-4 py-2 rounded hover:scale-105 transition transform"
-                            >
-                                {selectedListId === list.id ? 'Ocultar detalles' : 'Más detalles'}
-                            </button>
-                            {selectedListId === list.id && (
-                                <button
-                                    onClick={handleToggleGrid}
-                                    className="w-full md:w-auto bg-green-400 text-white px-4 py-2 rounded hover:scale-105 transition transform"
-                                >
-                                    {showGrid ? 'Ocultar cartas' : 'Añadir carta'}
-                                </button>
-                            )}
-                        </div>
-
-                        {selectedListId === list.id && (
-                            <>
-                                <div className="mt-6">
-                                    <h4 className="text-base md:text-lg font-bold mb-4">Cartas en esta lista:</h4>
-                                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                                        {list.cards.map((card) => (
-                                            <li key={card.id} className="bg-[#B1C8FE]/50 p-4 rounded-lg shadow">
-                                                <div className="flex flex-col p-2 md:flex-row md:justify-between">
-                                                    <div className="font-semibold text-gray-600 flex flex-col md:flex-row gap-4">
-                                                        <span>{card.id}</span>
-                                                        <span>{card.name}</span>
-                                                    </div>
-                                                    <button
-                                                        onClick={() => handleAddOrRemoveCard(card.id, true)}
-                                                        className="text-accent font-bold hover:underline self-start"
-                                                    >
-                                                        Eliminar
-                                                    </button>
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-
-                                {showGrid && (
-                                    <div className="mt-8 border-t pt-6">
-                                        <h4 className="text-base md:text-lg font-semibold text-primary mb-4">Añadir nueva carta</h4>
-                                        <TradableCardsGrid
-                                            onAddOrRemoveCard={handleAddOrRemoveCard}
-                                            currentCards={list.cards}
-                                        />
-                                    </div>
-                                )}
-                            </>
-                        )}
+                    
+                        <button
+                            onClick={() => openModal(list)}
+                            className="flex items-center gap-2 text-primary hover:scale-115 transition"
+                        >
+                            <Eye size={22} />
+                        </button>
                     </div>
                 ))}
             </div>
+
+            <CardListModal
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                initialSelectedList={selectedList}
+            />
         </div>
     );
 };
